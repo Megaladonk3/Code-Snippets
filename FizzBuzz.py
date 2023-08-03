e9f4fe07e005ea2a4dd7987acdff0e929c875761
Meghana Komuravelli <115751560+Megaladonk3@users.noreply.github.com>
2023-08-03 18:23:27 -0400

    Add files via upload
    Prints the numbers from 1 to 100, “Fizz” for multiples od 3, “Buzz” for multiples of 5 and “Fizz Buzz” for multiples of 15.

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+#For "Fizz" print numbers 1-100 for multiples of 3
+#For "Buzz" print numbers 1-100 for multiples of 5
+#For "FizzBuzz" print numbers 1-100 for multiples of 15
+
+for i in range(1, 101):
+    if i % 15 == 0:
+        print("FizzBuzz")
+        continue
+    elif i % 3 == 0:
+        print("Fizz")
+        continue
+    elif i % 3 == 0:
+        print("Fizz")
+        continue
+
+    print(i)
