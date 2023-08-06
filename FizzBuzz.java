a247e696061fff5a9402070f5af32310881bf730
Meghana Komuravelli <115751560+Megaladonk3@users.noreply.github.com>
2023-08-06 13:12:17 -0400

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,26 @@
+// Java program to print Fizz Buzz
+import java.util.*;
+class FizzBuzz
+{
+	public static void main(String[] args)
+	{
+		int n = 100;
+
+		// loop for 100 times
+		for (int i=1; i<=n; i++)								
+		{
+		
+			if (i%15==0)												
+				System.out.println("FizzBuzz");
+
+			else if (i%5==0)	
+				System.out.println("Buzz");
+
+			else if (i%3==0)	
+				System.out.println("Fizz");
+				
+			else // print the numbers
+				System.out.println(i);						
+		}
+	}
+}
