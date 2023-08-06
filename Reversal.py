06f0c31422f9e61886e489975d0c1f53a736dbaa
Meghana Komuravelli <115751560+Megaladonk3@users.noreply.github.com>
2023-08-06 13:35:54 -0400

    Add files via upload

---
+++ Reversal.py
@@ -0,0 +1,7 @@
+#Reverse the digits of a number
+numb = input("Enter any number: ")
+print("The reverse of your number is",str(numb)[::-1])
+
+#Reverse the order of a string
+numb = input("\nEnter any word or phrase: ")
+print("The reverse of your word or phrase is",str(numb)[::-1])
